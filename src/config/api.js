a315b275e549d9ee35b8a23305e75f5f4bfb454d
tprayogaa <tprayogaa26@gmail.com>
2022-05-17 01:30:31 +0700

    update

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const API = axios.create({
-  baseURL: process.env.SERVER_URL || "https://dumbmerch-v1.herokuapp.com/api/v1" || "http://localhost:5000/api/v1/",
+  baseURL: process.env.SERVER_URL || "https://dumb-merch-fedryan.herokuapp.com/api/v1" || "http://localhost:5000/api/v1/",
 });
 
 export const setAuthToken = (token) => {
